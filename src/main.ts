cc710347b5973d749a1939da5defbe6d45142cc2
0x00h <ronnachai45399@gmail.com>
2021-08-04 12:47:46 +0700

    add hehroku deployment

--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { join } from 'path';
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify';
-import * as utc from 'dayjs/plugin/utc'
-import * as timezone from 'dayjs/plugin/timezone'
-import * as isToday from 'dayjs/plugin/isToday'
-import * as dayjs from 'dayjs'
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import * as utc from 'dayjs/plugin/utc';
+import * as timezone from 'dayjs/plugin/timezone';
+import * as isToday from 'dayjs/plugin/isToday';
+import * as dayjs from 'dayjs';
 import secureSession from 'fastify-secure-session';
 
 async function bootstrap() {
+  dayjs.extend(utc);
+  dayjs.extend(timezone);
+  dayjs.extend(isToday);
+  dayjs.tz.setDefault('America/New_York');
 
-dayjs.extend(utc)
-dayjs.extend(timezone)
-dayjs.extend(isToday)
-dayjs.tz.setDefault("America/New_York")
-
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter({ logger: false }),
-  );
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter({ logger: false }));
   app.useStaticAssets({
     root: join(__dirname, 'web', 'public'),
     prefix: '/public/',
   });
   app.setViewEngine({
     engine: {
-      ejs: require('ejs')
+      ejs: require('ejs'),
     },
-    layout: "./layouts/main",
+    layout: './layouts/main',
     includeViewExtension: true,
     templates: join(__dirname, './web', 'views'),
   });
@@ -40,13 +33,14 @@ dayjs.tz.setDefault("America/New_York")
     salt: 'mq9hDxBVDbspDR6n',
     cookie: {
       path: '/',
-      httpOnly: true // Use httpOnly for all production purposes
+      httpOnly: true, // Use httpOnly for all production purposes
       // options for setCookie, see https://github.com/fastify/fastify-cookie
-    }
+    },
   });
 
+  app.enableCors();
 
-  await app.listen(process.env.PORT || 3000, '0.0.0.0')
-  console.log(`server is running on ${await app.getUrl()}`)
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
+  console.log(`server is running on ${await app.getUrl()}`);
 }
 bootstrap();

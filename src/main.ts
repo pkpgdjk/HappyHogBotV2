5ca48a2c2d1e6044f3d996d3bd0b7edc916fd844
0x00h <ronnachai45399@gmail.com>
2021-08-04 13:30:45 +0700

    enable log

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
   dayjs.extend(isToday);
   dayjs.tz.setDefault('America/New_York');
 
-  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter({ logger: false }));
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter({ logger: true }));
   app.useStaticAssets({
     root: join(__dirname, 'web', 'public'),
     prefix: '/public/',
